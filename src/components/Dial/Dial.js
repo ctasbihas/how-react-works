74ab7fa9379bda9d5aa9a1e7b62cf57531b3c67b
Chowdhury Tasbih Ahmed Siddique <ctasbihas>
2022-05-05 16:06:43 +0600

    last commit

--- src/components/Dial/Dial.js
+++ src/components/Dial/Dial.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Dial = (props) => {
     return (
-        <div style={{ border: '2px solid blue'}}>
+        <div style={{ border: '2px solid blue', margin: '5px' }}>
             <h3>This is Dial</h3>
             <p>steps so far: {props.steps}</p>
         </div>
